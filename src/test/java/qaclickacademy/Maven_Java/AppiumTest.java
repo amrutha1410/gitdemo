57d78576571b80be5b559ac33f68708ca917b592
amrutha1410 <amrutha14.m@gmail.com>
2020-11-25 23:56:42 -0500

    first commit

---
+++ src/test/java/qaclickacademy/Maven_Java/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Maven_Java;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeandroidapp()
+	{
+		System.out.println("nativeandroidapp");
+	}
+
+	@Test
+	public void IOSapps()
+	{
+		System.out.println("IOSapps");
+	}
+}
---
+++ src/test/java/qaclickacademy/Maven_Java/RestApiTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Maven_Java;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+
+	@Test
+	public void deletetwitter()
+	{
+		System.out.println("deletetwitter");
+	}
+}
